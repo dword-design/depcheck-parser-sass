fa89dcada8ec37ba9eb7565f21062ce85f26aaa8
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-05-26 11:28:37 +0200

    fix: foo

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
-import { compact, map, replace, uniq } from '@dword-design/functions'
+import { compact, map, uniq } from '@dword-design/functions'
 import getPackageName from 'get-package-name'
-import sass from 'node-sass'
-import importer from 'node-sass-package-importer'
-import P from 'path'
-import requirePackageName from 'require-package-name'
+import resolveCwd from 'resolve-cwd'
+import sass from 'sass'
+import { pathToFileURL } from 'url'
 
-export default filePath => {
-  const result = sass.renderSync({
-    file: filePath,
-    importer: importer({ cwd: process.cwd() }),
+export default path => {
+  const result = sass.compile(path, {
+    importers: [{ findFileUrl: url => url |> resolveCwd |> pathToFileURL }],
   })
 
   return (
-    result.stats.includedFiles
-    |> map(
-      path =>
-        path[0] === '~'
-          ? requirePackageName(path.substr(1))
-          : getPackageName(path |> replace(/\//g, P.sep)), // fix node-sass incorrect path format for windows
-    )
+    result.loadedUrls
+    |> map(url => getPackageName(url.pathname))
     |> compact
     |> uniq
   )
