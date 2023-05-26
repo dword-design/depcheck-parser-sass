d5dd665d9374813422773972044fc8bbf377ffb0
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-05-26 12:07:53 +0200

    fix: foo

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import { compact, map, uniq } from '@dword-design/functions'
 import getPackageName from 'get-package-name'
 import resolveCwd from 'resolve-cwd'
 import sass from 'sass'
-import { pathToFileURL } from 'url'
+import { fileURLToPath, pathToFileURL } from 'url'
 
 export default path => {
   const result = sass.compile(path, {
     importers: [{ findFileUrl: url => url |> resolveCwd |> pathToFileURL }],
   })
   console.log(result)
-  console.log(result.loadedUrls
-    |> map(url => getPackageName(url.pathname)))
+  console.log(result.loadedUrls |> map(url => fileURLToPath(url.href)))
+  console.log(result.loadedUrls |> map(url => getPackageName(url.pathname)))
 
   return (
     result.loadedUrls
